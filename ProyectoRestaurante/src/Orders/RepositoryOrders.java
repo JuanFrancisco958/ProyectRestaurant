1a670db41eebf0d3a05583fef90c8595e4641d1b
nicorrt <77391097+nicorrt@users.noreply.github.com>
2021-02-15 20:36:57 +0100

    Update RepositoryOrders.java

--- ProyectoRestaurante/src/Orders/RepositoryOrders.java
+++ ProyectoRestaurante/src/Orders/RepositoryOrders.java
@@ -1,48 +1,50 @@
 package Orders;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
-import Interfacez.
-/**
- * 
- * @author nicor
- *
- */
+
+
 public class RepositoryOrders {
 	
-	private List<IOrder> orders;
+	private List<Order> orders = new ArrayList<>();
 	
-	public RepositoryOrder(List<IOrder> orders) {
-		super();
+	public void RepositoryOrder(List<Order> orders) {
+		this.setOrders(orders);
+	}
+	
+	public void getAllOrders() {
+		orders.forEach(p->System.out.println());
+	}
+	
+	public List<Order> getOrdersByClient(String dni){
+		return null;
+	}
+	
+	public List<Order> getOrdersByDate(LocalDate ini, LocalDate end){
+		return null;
+	}
+	
+	public List<Order> getOrdersNoDelivered(){
+		return null;
+	}
+	
+	public List<Order> getOrdersNoPayed(){
+		return null;
+	}
+	
+	public List<Order> getAllInput() {
+		return null;
+	}
+	
+	public List<Order> getInputByDate (LocalDate ini, LocalDate end) {
+		return null;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
 		this.orders = orders;
-	}
-	
-	@Override
-	public List<IOrder> getAllOrders() {
-		return null;
-	}
-	
-	@Override
-	public List<IOrder> getOrdersByClient(String dni){
-		return null;
-	}
-	
-	public List<IOrder> getOrdersByDate(LocalDate ini, LocalDate end){
-		return null;
-	}
-	
-	public List<IOrder> getOrdersNoDelivered(){
-		return null;
-	}
-	
-	public List<IOrder> getOrdersNoPayed(){
-		return null;
-	}
-	
-	public List<IOrder> getAllInput() {
-		return null;
-	}
-	
-	public List<IOrder> getInputByDate (LocalDate ini, LocalDate end) {
-		return null;
 	}
 }
