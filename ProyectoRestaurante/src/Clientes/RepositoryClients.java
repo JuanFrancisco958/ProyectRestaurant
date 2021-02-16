34d8bc5e536cfb9b1219002feb540183656d56bc
raultc95 <78847181+raultc95@users.noreply.github.com>
2021-02-16 21:28:33 +0100

    Añadir funcionalidades al repositorio de clientes
    He actualizado la clase cliente, revisadlo por si hay algun fallo.

--- ProyectoRestaurante/src/Clientes/RepositoryClients.java
+++ ProyectoRestaurante/src/Clientes/RepositoryClients.java
@@ -1,35 +1,58 @@
-package Clases;
+package Clientes;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RepositoryClients {
+public class RepositoryClients implements Serializable{
 	ArrayList<Client> clients=new ArrayList<>();
 	
-	public String getAllClients() {
-		return null;
-		
+	public RepositoryClients() {
+		clients.add(new Client("50627461E", "Raul", 25));
+		clients.add(new Client("15451593D", "Jesus", 24));
+		clients.add(new Client("12345678Z", "Angel", 24));
 		
 	}
-	public String searchClient(String name) {
-		return name;
+	
+	public void getAllClients() {
+		clients.forEach(p->System.out.println(p));
+		
+	}
+	public Client searchClient(String name) {
+		for (Client client : clients) {
+			if (client.getName().equals(name)) {
+				return client;
+			}	
+		}
+		return null;
 		
 	}
 	//se puede poner Boolean para que diga si se ha hecho bien la operacion
-	public void updateClient(Client c) {
-		
+	public void updateClient(Client c, String dni) {
+		for (Client client : clients) {
+			if (client.getDni().equals(dni)) {
+				client=c;
+			}
+		}
 	}
-	public Client addClient(Client c) {
-		return c;
+	public void addClient(Client c) {
+		clients.add(c);
 		
 	}
 	//se puede poner Boolean para que diga si se ha hecho bien la operacion
 	public void deleteClient(String dni) {
-		
+		for (Client client : clients) {
+			if (client.getDni().equals(dni)){
+				clients.remove(client);
+			}
+		}
 	}
 	public Client searchClientDni(String dni) {
+		for (Client client : clients) {
+			if (client.getDni().equals(dni)) {
+				return client;
+			}
+		}
 		return null;
-		
 	}
-	
-
 }
+
